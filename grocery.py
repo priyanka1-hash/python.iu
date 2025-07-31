e13d23cb1de73d3d2f122ecdd2f45853daa3b4b8
priyanka1-hash <priyankamaheswarn1@gmail.com>
2025-07-31 15:54:03 +0530

    Create grocery.py

---
+++ grocery.py
@@ -0,0 +1,8 @@
+grocery_item=["milk",'curd','chilly']
+for i in grocery_item:
+    print (i)
+
+grocery_item=["milk",'curd','chilly']
+for i, item in enumerate(grocery_item):
+    print (i+1,item)
+    print ('end')
