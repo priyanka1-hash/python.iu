46a0c2d3fa121d99bfe5bacb1d1dfeb7ba036815
priyanka1-hash <priyankamaheswarn1@gmail.com>
2025-07-31 15:50:49 +0530

    Create schedule.py

---
+++ schedule.py
@@ -0,0 +1,4 @@
+training_schedule=["computer", "python","loop", "cloud","cybersecurity"]
+print("My 5-day training schedule:")
+for day, topic in enumerate (training_schedule, start=1):
+    print(day,topic)
